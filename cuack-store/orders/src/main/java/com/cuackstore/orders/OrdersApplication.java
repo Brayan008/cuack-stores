948b01885fa4d13e4aea14f7de915ad0207961b8
Brayan Mares <67080870+Brayan008@users.noreply.github.com>
2025-08-07 21:03:02 -0600

    Fix feign

--- cuack-store/orders/src/main/java/com/cuackstore/orders/OrdersApplication.java
+++ cuack-store/orders/src/main/java/com/cuackstore/orders/OrdersApplication.java
@@ -3,8 +3,10 @@ package com.cuackstore.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"com.cuackstore.*"})
 public class OrdersApplication {
 
